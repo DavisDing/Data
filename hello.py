048a2c2afa8f05208fbdec5e233cff0884862b99
dinghao <davis.ding@aliyun.com>
2019-08-02 10:57:29 +0800

    update

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
-i=10
-print(i)
+# -*- coding:utf-8 -*-
+import ibm_db
+conn = ibm_db.connect("DATABASE=dbname;HOSTNAME=localhost;PORT=50000;PROTOCOL=TCPIP;UID=usename;PWD=password;", "", "")
+if conn:
+    sql = "SELECT * FROM dual"
+    stmt = ibm_db.exec_immediate(conn, sql)
+    result = ibm_db.fetch_both(stmt)
+    while( result ):
